f1f91a23eb393e887dc86b2a2785141226dac640
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-06 19:41:12 -0400

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse # calling the HttpResponse method
 
 
-# Create your views here. 
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+# Create your views here.
+def index(request):
+    return HttpResponse("Index")
+
+def hello(request,username):
+    return HttpResponse("<h1>Hello %s</h1>" % username) # here a param
 
 def about(request):
     return HttpResponse("<h1>About</h1>")
