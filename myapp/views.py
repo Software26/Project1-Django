f0336a45b4fb926a9adbd4a6c2e43de3d7b70fde
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-06 08:55:38 -0400

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse # calling the HttpResponse method
 
 
 # Create your views here. 
 def hello(request):
     return HttpResponse("<h1>Hello World</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About</h1>")
