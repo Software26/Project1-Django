c5db06c9b669fd6d8f24b90e93b9dddb6b260664
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-06 09:32:24 -0400

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from myapp.views import hello 
-from myapp.views import about
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',hello),# '' --- principal rute
-    path("about/",about)
+    path('', include('myapp.urls'))
+    
 ]
