eb9c4c97289835af830669a8b0d9576807c49947
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-06 20:04:47 -0400

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse # calling the HttpResponse method
-
+from django.http import HttpResponse, JsonResponse # calling the HttpResponse method
+from .models import Project # calling bd
 
 # Create your views here.
 def index(request):
     return HttpResponse("Index")
 
 def hello(request,username):
-    return HttpResponse("<h1>Hello %s</h1>" % username) # here a param
+    return HttpResponse("<h1>Hello %s </h1>" % username) # here a param
 
 def about(request):
     return HttpResponse("<h1>About</h1>")
+
+def project(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request):
+    return HttpResponse("Tasks")
