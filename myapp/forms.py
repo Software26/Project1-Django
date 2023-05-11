9bebdfe28272583a1233871fb651bb8ef948166b
Natanael Sosa <51962394+Software26@users.noreply.github.com>
2023-05-11 18:49:34 -0400

    Update forms.py

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 
 class CreateNewTask(forms.Form):
-    title = forms.Charfield(label="Tutulo de tarea", max_length = 200)
-    description = forms.Textarea(with)
+    title = forms.CharField(label="Tutulo de tarea", max_length = 200)
+    
+    description = forms.CharField(label = "Descripcion de la tarea", widget=forms.Textarea)
